21319d9b6f94d028c4a8e79e4cd5e0e820b7d322
Isaac Marcos <isaacmarcos@scdesign.com.br>
2023-02-25 08:01:39 -0300

    Listagem dos Posts no Blog

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,15 +1,19 @@
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
-import { GetStaticProps } from "next";
 import { getPrismicClient } from "../../services/prismic";
+import { useEffect, useState } from "react";
 
 type Post = {
   slug: string;
   title: string;
+  tag: string;
+  banner: {
+    url: string;
+  };
   excerpt: string;
   updatedAt: string
 };
@@ -18,7 +22,45 @@ interface PostsProps {
   posts: Post[]
 }
 
-export function Blog({ posts }: PostsProps) {
+export function Blog() {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [pageSize, setPageSize] = useState(5);
+
+  const getBlogPosts = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+      Prismic.predicates.at('document.type', 'post')
+    ], {
+      fetch: ['post.title', 'post.banner', 'post.tag', 'post.content'],
+      pageSize: pageSize,
+      orderings:'[document.last_publication_date desc]'
+    })
+
+    const formattedPosts = response.results.map(post => {
+      return {
+        slug: post.uid!,
+        banner: {
+          url: post.data.banner.url,
+        },
+        tag: post.data.tag,
+        title: RichText.asText(post.data.title),
+        excerpt: post.data.content.find((content: { type: string }) => content.type === 'paragraph')?.text ?? '',
+        updatedAt: post.last_publication_date ? new Date(post.last_publication_date).toLocaleDateString('pt-BR', { 
+          day: '2-digit',
+          month: 'short',
+          year: 'numeric'
+        })
+        : ''
+      }
+    })
+    setPosts(formattedPosts)
+  }
+  
+  useEffect(() => {
+    getBlogPosts()
+  }, [pageSize])
+
   return (
     <Flex direction="column" align="center" justify="center">
       <Header />
@@ -33,35 +75,57 @@ export function Blog({ posts }: PostsProps) {
             <Flex w={{ lg:"900px", md:"100%", base:"100%" }} flexDirection={{ lg:"column", base:"column" }} gap="2rem">
             
               {posts.map(post => (
-                <Box key={post.slug} display={{ lg: "flex", md: "flex", base: "table-column" }} lineHeight="1.5rem" pb="1.875rem" borderBottom="1px" borderColor="gray.200" >
-                  <Image display="block" w={{ lg: "295px", md: "295px", base: "100%" }} h="182px" mr="25px" objectFit="cover" src="https://arquivos.padrepauloricardo.org/uploads/destaque_do_blog/imagem/2038/cardeal-pell-relacionados.jpg"
-                  />
-                  <Flex flexDirection="column" p="0.5rem" >
+                <Box key={post.slug} display={{ lg: "flex", md: "flex", base: "table-column" }} lineHeight="1.5rem" pb="1.875rem" borderBottom="1px" borderColor="gray.200"  >
+                  <Flex bgColor="black" w={{ lg: "295px", md: "295px", base: "100%" }} minW={{ lg: "295px", md: "295px", base: "100%" }} h="185px">
+                    <Link href={`/blog/${post.slug}`} target="_blank" >
+                    <Image w="100%" h="100%" objectFit="cover" src={post.banner.url} _hover={{ opacity: 0.7 }} transition="opacity 0.5s"
+                    />
+                    </Link>
+                  </Flex>
+                  <Flex as={Link} href={`/blog/${post.slug}`}
+                    style={{ textDecoration: "none" }}
+                    sx={{
+                      textDecoration: "none",
+                      "&:hover h2": {
+                        color: "yellow.700",
+                      },
+                      "& h2": {
+                        transition: "color 0.2s ease-in-out "
+                      }
+                    }} target="_blank"
+                    flexDirection="column" px="0.5rem" ml="25px"
+                  >
                     <Flex fontSize="0.75rem" w="100%" >
-                      <Text color="yellow.800" fontWeight="700" mr="0.625rem" textTransform="uppercase" >
-                        DOUTRINA
+                      <Text color="yellow.800" fontWeight="700" mr="0.625rem" textTransform="uppercase">
+                        {post.tag}
                       </Text>
                       <Text color="gray.700" fontWeight="500" fontSize="0.8125rem" textTransform="lowercase" >
-                        | {post.updatedAt}  17.jan.2023
+                         | {post.updatedAt}  {/* 17.jan.2023 */}
                       </Text>
                     </Flex>
                     <Heading color="gray.900" fontSize="1.375rem" my="0.5rem" lineHeight="1.6875rem" mb=".375rem">
                       {post.title}
-                      O Cardeal Pell sobre o Céu e o Inferno
                     </Heading>
-                    <Text color="gray.600" fontSize="1.0625rem" lineHeight="1.5625rem">
-                      {post.excerpt}
-                      “Toda a nossa atividade sacerdotal é determinada pelo número de pessoas que acreditamos que serão salvas. Se não há punição e todos serão salvos, por que deveríamos nos preocupar? Por que Jesus se incomodou com a Cruz?”
+                    <Text color="gray.600" fontSize="1.0625rem" lineHeight="1.5625rem" white-space="nowrap" text-overflow="ellipsis" maxW="100%" overflow="hidden" display="-webkit-box" letterSpacing="-0.025rem" noOfLines={4}  >
+                      {post.excerpt}...
                     </Text>
                   </Flex>
                 </Box>
               ))}
-                
+                <Button onClick={() => setPageSize(pageSize + 5)} fontSize="1.125rem" p="1.3125rem" h="60px" bgColor="yellow.500" color="gray.700" _hover={{ bgColor:"yellow.700", color:"gray.50" }} >
+                  Carregar mais
+                </Button>
             </Flex>
             
-            <Box display={{ lg: "flex", md: "none", base: "none" }} w="450px" h="700px" bgImage="/Lepanto.jpg" bgSize="cover" bgPosition="center" objectFit="contain" bgColor="gray.500" ml="50px">
+            <Flex display={{ lg: "flex", md: "none", base: "none" }} direction="column" w="450px" ml="50px" align="center" >
+
+              <Box w="100%" h="700px" bgImage="/Lepanto.jpg" bgSize="cover" bgPosition="center" objectFit="contain" bgColor="gray.500"  />
+
+              <Text fontFamily="Gentium" mt="1rem" textAlign="center" fontSize="16px" w="90%" letterSpacing="-0.025rem">
+                Dê-me, Senhor, agudeza para entender, capacidade para reter, método e faculdade para aprender, sutileza para interpretar, graça e abundância para falar. Dê-me, Senhor, acerto ao começar, direção ao progredir, e perfeição ao concluir. Amém.
+              </Text>
               
-            </Box>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
@@ -69,36 +133,4 @@ export function Blog({ posts }: PostsProps) {
       <Footer />
     </Flex>
   )
-}
-
-
-export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient()
-
-  const response = await prismic.query([
-      Prismic.predicates.at('document.type', 'post')
-  ], {
-      fetch: ['post.title', 'post.content'],
-      pageSize: 100,
-  })
-
-  const posts = response.results.map(post => {
-      return {
-          slug: post.uid,
-          title: RichText.asText(post.data.title),
-          excerpt: post.data.content.find((content: { type: string; }) => content.type === 'paragraph')?. text ?? '',
-          updatedAt: post.last_publication_date ? new Date (post.last_publication_date).toLocaleDateString('pt-BR', {
-              day: '2-digit',
-              month: 'long',
-              year: 'numeric'
-          })
-          : ''
-      }
-  }) 
-
-  return {
-      props: {
-          posts
-      }
-  }
 }
